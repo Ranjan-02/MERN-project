bc285b8888168d45d4c698b7d5132e60664a631b
Ranjan02/ <ranjanmaji098@gmail.com>
2023-09-27 18:51:20 +0530

    start creating ecommerce full features project

---
+++ ecommerce-app/server.js
@@ -0,0 +1,24 @@
+// Bring express
+
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv"
+
+//asign express
+
+const app = express();
+
+dotenv.config()
+
+app.get("/", (req, resp) => {
+    resp.send({
+        success: true
+    })
+})
+
+// PORT 
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(colors.bgYellow.black(`server running ${process.env.MODE} port no ${PORT}`));
+})
